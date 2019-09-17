b11ae2b3b0611264ea2ecbdccd5edd0dfae7af98
Roy Nyaga <nyagaandreroy@gmail.com>
2019-09-17 03:09:07 +0100

    styled static pages, added links and tested

--- sample_app/test/controllers/static_pages_controller_test.rb
+++ sample_app/test/controllers/static_pages_controller_test.rb
@@ -12,19 +12,19 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   # end
 
   test "should get help" do
-    get static_pages_help_url
-    assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    get help_url
+    assert_response :success # check if the get help_url is a success code
+    assert_select "title", "Help | #{@base_title}" # check if there is a title tag with the following content.....
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_url
     assert_response :success
     assert_select "title", "About | #{@base_title}"
   end
 
   test "should get contact" do
-    get static_pages_contact_url
+    get contact_url
     assert_response :success
     assert_select "title", "Contact | #{@base_title}"
   end
