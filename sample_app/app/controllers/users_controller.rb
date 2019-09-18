52f046ea7f59c445402814704abc125a70be8083
Roy Nyaga <nyagaandreroy@gmail.com>
2019-09-18 04:51:24 +0100

    ended with 7.1.3 debugger

--- sample_app/app/controllers/users_controller.rb
+++ sample_app/app/controllers/users_controller.rb
@@ -1,4 +1,9 @@
 class UsersController < ApplicationController
   def new
   end
+
+  def show
+    @user = User.find(params[:id]) # params stands for parameters associated with the request url
+  end
+
 end
