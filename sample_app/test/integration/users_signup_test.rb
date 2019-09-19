dcfb19f190a4280222469f0d6b8ea4aabb74f869
Roy Nyaga <nyagaandreroy@gmail.com>
2019-09-19 02:50:50 +0100

    done with chapter 7

---
+++ sample_app/test/integration/users_signup_test.rb
@@ -0,0 +1,29 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name:  "",
+                                         email: "user@invalid",
+                                         password:              "foo",
+                                         password_confirmation: "bar" } }
+    end
+    assert_template 'users/new'
+    assert_select "div#error_explanation" #test for the presence of a div with id=erro_explanation on users/new html page
+    assert_select 'div.alert' #test for the presece of a div with class alert on users/new html page
+  end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "password",
+                                         password_confirmation: "password" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+  end
+end
--- sample_app/test/models/user_test.rb
+++ sample_app/test/models/user_test.rb
@@ -2,8 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com", 
-      password: "foobar", password_confirmation: "foobar")
+    @user = User.new(name: "Example User", email: "user@example.com")
   end
 
   test "should be valid" do
@@ -53,23 +52,6 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
-  end
-
-  test "email addresses should be saved as lower-case" do
-    mixed_case_email = "Foo@ExAMPle.CoM"
-    @user.email = mixed_case_email
-    @user.save
-    assert_equal mixed_case_email.downcase, @user.reload.email
-  end
-
-  test "password should be present (nonblank)" do
-    @user.password = @user.password_confirmation = " " * 6
-    assert_not @user.valid?
-  end
-
-  test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
   end
 
 end
